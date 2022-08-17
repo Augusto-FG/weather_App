41472101e6678d7f0fc89ee5457dd868c178bee6
Augusto Fernández <79116699+Augusto-FG@users.noreply.github.com>
2022-08-17 19:47:39 -0300

    add files

---
+++ WeatherApp/Model/clima.swift
@@ -0,0 +1,14 @@
+//
+//  Wheather.swift
+//  WeatherApp
+//
+//  Created by Estefania Sassone on 04/08/2022.
+//
+
+import Foundation
+
+struct clima{
+    var temp: Double
+    var tempMax: Double
+    var tempMin: Double
+}
